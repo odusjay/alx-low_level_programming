4803e50ed31247712b29e8709cffa5e92f2b57fe
odusjay <odusjay1@gmail.com>
2022-09-08 19:20:21 -0700

    C programming task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - entry point.
+ * return - 1 on success.
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora Korpa, 2015-19-19\n", 59);
+	return (1);
+}
+
