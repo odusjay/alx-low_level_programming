e78f467db820b0607a5665fbc32b1a6d8affc787
odusjay <odusjay1@gmail.com>
2022-09-08 17:15:22 -0700

    c programming task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-serve as the entry point for the program
+ *
+ * Return:returns zero if no errors otherwise returns non-xero value.
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
