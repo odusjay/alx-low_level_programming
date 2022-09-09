fa32e9e1710aa8b35363621970912c4c8006f81b
odusjay <odusjay1@gmail.com>
2022-09-08 19:34:53 -0700

    C programming task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  * main - entry point.
  * return - 1 on success.
  */
-int main(void)
+void main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
