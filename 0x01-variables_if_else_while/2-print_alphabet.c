55587d1c2952f21c1f665bbf72776027869e22f8
odusjay <odusjay1@gmail.com>
2022-09-12 13:32:24 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-prt the alphabet in lowecase,
+ * only putchar is allowed
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l = 'a';
+
+	while (l < = 'z')
+	{
+	putchar(l);
+	l += 1;
+	}
+	putchar('\n');
+	return (0);
+}
