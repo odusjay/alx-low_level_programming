4e0efb7c314ed176169d46051d7d995f13eaa00c
odusjay <odusjay1@gmail.com>
2022-09-18 06:33:00 -0700

    variable if else task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print double digit combos
+ * Description: print double digit combos
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+	while (a < 58)
+	{
+	b = a + 1;
+	while (b < 58)
+	{
+	putchar(a);
+	putchar(b);
+	if (a < 56 || b < 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	b++;
+	}
+	a++;
+	}
+	putchar(10);
+	return (0);
+}
