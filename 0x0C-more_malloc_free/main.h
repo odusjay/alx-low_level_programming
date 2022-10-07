6623cd413b20938cf448f9da9b56a63d7450334c
odusjay <odusjay1@gmail.com>
2022-10-07 11:55:53 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,18 @@
-#ifndef _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#define _MAIN_H_
+void _print(char *str, int l);
 
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 
+int check_for_digits(char **av);
+
+void init(char *str, int l);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
@@ -14,4 +24,4 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+#endif/*MAIN_H*/
