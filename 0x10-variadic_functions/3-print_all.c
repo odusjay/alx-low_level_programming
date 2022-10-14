0711dbc1ad83d0079b12871cdddc5fd239eedfaf
odusjay <odusjay1@gmail.com>
2022-10-13 17:33:20 -0700

    Variadic function task 0-3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,17 +1,14 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
  * Return: no return
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
-
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
